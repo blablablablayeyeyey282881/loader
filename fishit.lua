a685889b6c470fc4e63ac8acc1cb258128785094
blablablablayeyeyey282881 <nexahub9@gmail.com>
2025-10-01 21:04:19 +0900

    Update fishit.lua

--- fishit.lua
+++ fishit.lua
@@ -8,7 +8,7 @@ if placeId == 121864768012064 then
     success = true
 elseif placeId == 127742093697776 then
     gameName = "Plant Vs Brainrots"
-    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/026e7028c3afabd0"))()
+    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/79daadea2fa4eff8"))()
     success = true
 elseif placeId == 18687417158 then
     gameName = "Forsaken"
